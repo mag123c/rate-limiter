1a23cbe24725c31d8ea27c53c5efbfe7c0205c75
mag123c <diehreo@gmail.com>
2025-08-03 20:40:06 +0900

    refactor: esm

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,15 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["<rootDir>"],
-  testMatch: ["**/*.spec.ts"],
-  transform: {
-    "^.+\\.ts$": [
-      "ts-jest",
-      {
-        tsconfig: {
-          module: "ES2022",
-        },
+export const preset = "ts-jest";
+export const testEnvironment = "node";
+export const roots = ["<rootDir>"];
+export const testMatch = ["**/*.spec.ts"];
+export const transform = {
+  "^.+\\.ts$": [
+    "ts-jest",
+    {
+      tsconfig: {
+        module: "ES2022",
       },
-    ],
-  },
-  moduleFileExtensions: ["ts", "js"],
+    },
+  ],
 };
+export const moduleFileExtensions = ["ts", "js"];
